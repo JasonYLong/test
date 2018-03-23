ac3600834540b49314d1a4726d733c5c13f25c7b
Yuan Long <long.yuan@foxmail.com>
2018-03-23 23:37:57 +0800

    add df command

--- test.py
+++ test.py
@@ -5,9 +5,13 @@ import os
 def backup_data():
     shell_str= 'find /tmp/test -name "*.gzip" -mtime -1 | xargs -I {} cp {} /tmp/backup'
     os.system(shell_str)
+    
+def df():
+    os.system('df -h')
 
 def run():
     backup_data()
+    df()
 
 if __name__ == '__main__':
     run()
